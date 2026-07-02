0193fb4f0926f0df586488aaa914c45de10f7905
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-07-02 09:48:33 +0300

    Add links to books txt files

--- render_website.py
+++ render_website.py
@@ -1,10 +1,14 @@
 import json
 from more_itertools import chunked
+from urllib.parse import quote
 
 from jinja2 import Environment, FileSystemLoader
 
 with open("books/meta_data.json", encoding="utf-8") as file:
     books = json.load(file)
+
+for book in books:
+    book["book_url"] = quote("books/" + book["book_path"])
 
 env = Environment(loader=FileSystemLoader("templates"))
 template = env.get_template("index.html")
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import json
+from urllib.parse import quote
+from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader
 from livereload import Server
 
@@ -7,10 +9,13 @@ def on_reload():
     with open("books/meta_data.json", encoding="utf-8") as file:
         books = json.load(file)
 
+    for book in books:
+        book["book_url"] = quote("books/" + book["book_path"])
+
     env = Environment(loader=FileSystemLoader("templates"))
     template = env.get_template("index.html")
 
-    html = template.render(books=books)
+    html = template.render(books=list(chunked(books, 2)))
 
     with open("index.html", "w", encoding="utf-8") as file:
         file.write(html)
